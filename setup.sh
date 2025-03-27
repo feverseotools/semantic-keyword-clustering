56c2c08e3313f48988133e0a6292bc60c07abbc0
Fever SEO <seo.tools@feverup.com>
2025-03-27 12:37:59 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Download SpaCy language models
+python -m spacy download en_core_web_sm
+python -m spacy download es_core_news_sm
+python -m spacy download fr_core_news_sm
+python -m spacy download de_core_news_sm
+python -m spacy download it_core_news_sm
+python -m spacy download pt_core_news_sm
