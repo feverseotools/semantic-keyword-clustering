d6c355f7190df3e52fc26ac3a6af2c5e59e77894
Fever SEO <seo.tools@feverup.com>
2025-05-14 13:55:23 +0200

    Delete export directory

--- export/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from export_pdf import PDFReport, add_pdf_export_button, create_download_link, sanitize_filename
-
-__all__ = ['PDFReport', 'add_pdf_export_button', 'create_download_link', 'sanitize_filename']
